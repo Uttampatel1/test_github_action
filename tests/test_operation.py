e29d2328c83d71abbfd662dca22438fe400f40a2
Uttam Patel <86312108+Uttampatel1@users.noreply.github.com>
2024-09-05 19:49:43 +0530

    Update test_operation.py

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,27 +1,23 @@
 from src.math_operations import add, subtract, multiply, divide
-import unittest
+import pytest
 
-class TestMyFunctions(unittest.TestCase):
-    
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(0, 0), 0)
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
 
-    def test_subtract(self):
-        self.assertEqual(subtract(5, 3), 2)
-        self.assertEqual(subtract(0, 5), -5)
-        self.assertEqual(subtract(-5, -3), -2)
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 5) == -5
+    assert subtract(-5, -3) == -2
 
-    def test_multiply(self):
-        self.assertEqual(multiply(4, 5), 20)
-        self.assertEqual(multiply(0, 5), 0)
-        self.assertEqual(multiply(-4, 5), -20)
+def test_multiply():
+    assert multiply(4, 5) == 20
+    assert multiply(0, 5) == 0
+    assert multiply(-4, 5) == -20
 
-    def test_divide(self):
-        self.assertEqual(divide(10, 2), 5)
-        self.assertEqual(divide(-10, 2), -5)
-        self.assertRaises(ValueError, divide, 10, 0)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_divide():
+    assert divide(10, 2) == 5
+    assert divide(-10, 2) == -5
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide(10, 0)
